0b65cd3a3080140d7776d71706a9a862fe5123b5
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2015-08-26 04:05:33 -0300

    Ajustes na comparação dos horários
    Foi ajustado os horários, para adequar ao timezone local e não utc.
    Aproveitando, foi ajustado a forma de persistir os valores de conexão,
    para que seja possível realizar auditoria do log de acesso

--- lib/devise/restrictable/model.rb
+++ lib/devise/restrictable/model.rb
@@ -33,15 +33,17 @@ module Devise
       end
 
       def set_to_connected!(request)
-        update_column :connected_at, Time.now.utc
-        update_column :connected_ip, request.remote_ip
-        update_column :updated_at, Time.now.utc
+        write_attribute :connected_at, Time.now.utc
+        write_attribute :connected_ip, request.remote_ip
+        write_attribute :updated_at, Time.now.utc
+        save validate: false
       end
 
       def set_to_disconnected!
-        update_column :connected_at, nil
-        update_column :connected_ip, nil
-        update_column :updated_at, Time.now.utc
+        write_attribute :connected_at, nil
+        write_attribute :connected_ip, nil
+        write_attribute :updated_at, Time.now.utc
+        save validate: false
       end
 
       def connected_message
@@ -69,7 +71,7 @@ module Devise
       def in_work_time?
         return false unless work_start_time && end_time_work
 
-        now        = I18n.l Time.now.utc, format: time_format
+        now        = I18n.l Time.now, format: time_format
         start_time = I18n.l self.work_start_time, format: time_format
         end_time   = I18n.l self.end_time_work, format: time_format
 
@@ -79,7 +81,7 @@ module Devise
       def in_interval?
         return false unless start_of_interval && end_of_interval
 
-        now        = I18n.l Time.now.utc, format: time_format
+        now        = I18n.l Time.now, format: time_format
         start_time = I18n.l self.start_of_interval, format: time_format
         end_time   = I18n.l self.end_of_interval, format: time_format
 
